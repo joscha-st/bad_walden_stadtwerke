ca6482c8d453e9994f17dc9e5f7fcf383137db44
LukasStrickler <email@lukasstrickler.com>
2024-04-15 10:18:05 +0200

    refactor: Refactor Sidebar component to be more CLEAN

--- Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/components/Sidebar.java
+++ Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/components/Sidebar.java
@@ -5,61 +5,95 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.VBox;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Sidebar extends VBox {
+    private static final String TREE_VIEW_FONT_SIZE = "-fx-font-size: 16;";
+    private static double SIDEBAR_TREE_VIEW_HEIGHT;
+    private static final String SIDEBAR_TREE_BORDER = "-fx-border-color: black; -fx-border-width: 0 2 0 0;";
+
     public Sidebar() {
-        this.setPrefWidth(270); // Set the width of the sidebar to 270
-        this.setPrefHeight(720);
-        this.setStyle("-fx-border-color: black; -fx-border-width: 0 2 0 0;");
+        setPrefSize(270, 720);
+        SIDEBAR_TREE_VIEW_HEIGHT = getPrefHeight() - 30;
 
-        TreeItem<String> rootItem = new TreeItem<>("Root");
+        setStyle(SIDEBAR_TREE_BORDER);
 
-        TreeItem<String> homeItem = new TreeItem<>("Home");
-        rootItem.getChildren().add(homeItem);
-        homeItem.setExpanded(true);
+        TreeItem<String> rootItem = createRootItem();
 
-        TreeItem<String> stadtwerkeItem = new TreeItem<>("Stadtwerke");
-        stadtwerkeItem.getChildren().addAll(
-                new TreeItem<>("Wasser"),
-                new TreeItem<>("Strom"),
-                new TreeItem<>("Gas"),
-                new TreeItem<>("Rechnungen")
-        );
-        rootItem.getChildren().add(stadtwerkeItem);
-        stadtwerkeItem.setExpanded(true);
+        getChildren().addAll(createTreeView(rootItem), createLogoutButton());
+    }
 
-        TreeItem<String> adminItem = new TreeItem<>("Admin");
-        TreeItem<String> stadtwerkeAdminItem = new TreeItem<>("Stadtwerke");
-        TreeItem<String> vertraegeItem = new TreeItem<>("Verträge");
-        vertraegeItem.getChildren().addAll(
-                new TreeItem<>("Wasser"),
-                new TreeItem<>("Strom"),
-                new TreeItem<>("Gas")
-        );
-        stadtwerkeAdminItem.getChildren().addAll(vertraegeItem, new TreeItem<>("Kunden"));
-        adminItem.getChildren().add(stadtwerkeAdminItem);
-        rootItem.getChildren().add(adminItem);
-        adminItem.setExpanded(true);
-        stadtwerkeAdminItem.setExpanded(true);
-        vertraegeItem.setExpanded(true);
+    private TreeItem<String> createRootItem() {
+        return createTreeItem("Root", Arrays.asList(createHomeItem(), createCityServicesItem(), createAdminItem()));
+    }
 
-        rootItem.setExpanded(true);
+    private TreeItem<String> createHomeItem() {
+        return createTreeItem("Home", List.of());
+    }
 
+    private TreeItem<String> createCityServicesItem() {
+        return createTreeItem("City Services", createServiceItems("Water", "Electricity", "Gas", "Bills"));
+    }
+
+    private TreeItem<String> createAdminItem() {
+        return createTreeItem("Admin", List.of(createCityServicesAdminItem()));
+    }
+
+    private TreeItem<String> createCityServicesAdminItem() {
+        return createTreeItem("City Services", Arrays.asList(createContractsItem(), createTreeItem("Customers", List.of())));
+    }
+
+    private TreeItem<String> createContractsItem() {
+        return createTreeItem("Contracts", createServiceItems("Water", "Electricity", "Gas"));
+    }
+
+    private TreeItem<String> createTreeItem(String name, List<TreeItem<String>> children) {
+        TreeItem<String> item = new TreeItem<>(name);
+        item.getChildren().addAll(children);
+        item.setExpanded(true);
+        return item;
+    }
+
+    private List<TreeItem<String>> createServiceItems(String... services) {
+        return Stream.of(services).map(TreeItem::new).collect(Collectors.toList());
+    }
+
+    private boolean isAdminPage(TreeItem<String> item) {
+        while (item != null) {
+            if (item.getValue().equals("Admin")) {
+                return true;
+            }
+            item = item.getParent();
+        }
+        return false;
+    }
+
+    private TreeView<String> createTreeView(TreeItem<String> rootItem) {
         TreeView<String> treeView = new TreeView<>(rootItem);
         treeView.setShowRoot(false);
-        treeView.setStyle("-fx-font-size: 16;");
+        treeView.setStyle(TREE_VIEW_FONT_SIZE);
+        treeView.setPrefHeight(SIDEBAR_TREE_VIEW_HEIGHT);
+        setTreeViewActionListener(treeView);
+        return treeView;
+    }
+
+    private void setTreeViewActionListener(TreeView<String> treeView) {
         treeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
-                System.out.println("Clicked on: " + newValue.getValue());
+                String selectedPage = newValue.getValue();
+                String typePage = isAdminPage(newValue) ? "Admin" : "User";
+                System.out.println("Selected page "+typePage+": " + selectedPage);
             }
         });
-        treeView.setPrefHeight(690);
+    }
 
+    private Button createLogoutButton() {
         Button logoutButton = new Button("Logout");
-        logoutButton.setStyle("-fx-font-size: 16;");
+        logoutButton.setStyle(TREE_VIEW_FONT_SIZE);
         logoutButton.setOnAction(event -> System.out.println("Clicked on: Logout"));
-
-        this.getChildren().addAll(treeView, logoutButton);
-
-
+        return logoutButton;
     }
 }
