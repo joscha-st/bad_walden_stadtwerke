e00ed0b1409b43548386b65ba8d12cde94cfd92f
LukasStrickler <email@lukasstrickler.com>
2024-04-15 10:55:29 +0200

    feat: Refactor MainContent and Main classes
    - Added MainContent class to handle main content display
    - Refactored Main class to use MainContent instead of Label
    - Created separate methods for creating root and setting up stage

--- Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/Main.java
+++ Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/Main.java
@@ -1,28 +1,36 @@
 package com.bad_walden_stadtwerke;
 
+import com.bad_walden_stadtwerke.components.MainContent;
 import com.bad_walden_stadtwerke.components.Sidebar;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final double SCENE_WIDTH = 1280;
+    private static final double SCENE_HEIGHT = 720;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) {
+        HBox root = createRoot();
+        setupStage(stage, root);
+    }
+
+    private HBox createRoot() {
         Sidebar sidebar = new Sidebar();
+        MainContent mainContent = new MainContent();
+        HBox root = new HBox();
+        root.getChildren().addAll(sidebar, mainContent);
+        return root;
+    }
 
-        Label mainContent = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-        mainContent.setStyle("-fx-padding: 10;");
-
-        HBox contentArea = new HBox(sidebar, mainContent);
-
-        stage.setScene(new Scene(contentArea, 1280, 720));
-
+    private void setupStage(Stage stage, HBox root) {
+        stage.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT));
         stage.show();
     }
 }
