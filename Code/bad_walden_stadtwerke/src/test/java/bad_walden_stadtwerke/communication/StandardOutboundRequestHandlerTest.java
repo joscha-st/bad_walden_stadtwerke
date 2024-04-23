fd4b4ecb1e908b04b1faf9f842f650cd80716a47
i568830 <joscha.staehle@sap.com>
2024-04-23 11:09:38 +0200

    adjusted tests

--- Code/bad_walden_stadtwerke/src/test/java/bad_walden_stadtwerke/communication/StandardOutboundRequestHandlerTest.java
+++ Code/bad_walden_stadtwerke/src/test/java/bad_walden_stadtwerke/communication/StandardOutboundRequestHandlerTest.java
@@ -2,18 +2,24 @@ package bad_walden_stadtwerke.communication;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
 
 import com.bad_walden_stadtwerke.communication.StandardOutboundRequestHandler;
 import com.bad_walden_stadtwerke.sales.types.Tariff;
 
+/**
+ * Unit test {@link StandardOutboundRequestHandler}
+ */
 public class StandardOutboundRequestHandlerTest {
 
     @Test
-    void testMakeStandardOutboundRequest() throws Exception {
+    void testMakeStandardOutboundRequest() {
         String expectedResponse = "{\"Hello World\"}";
 
         String actualResponse = StandardOutboundRequestHandler.makeStandardOutboundRequest("test", "https://request-handling.int.bad-walden-stadtwerke.com/test");
@@ -22,12 +28,11 @@ public class StandardOutboundRequestHandlerTest {
     }
 
     @Test
-    void testMakeTariffOutboundRequest() throws Exception {
-        // List<Tariff> expectedResponse = "{\"Hello World\"}";
-
+    void testMakeTariffOutboundRequest() {
         List<Tariff> actualResponse = StandardOutboundRequestHandler.makeTariffOutboundRequest("electricity");
 
-        // assertEquals(expectedResponse, actualResponse);
+        assertInstanceOf(List.class, actualResponse);
+        assertTrue(actualResponse.stream().allMatch(Objects::nonNull));
     }
 
 }
