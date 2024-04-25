132e3e50796ffa7382cb1b8f75f301cf7799d3ed
Lukas Strickler <145582491+LukasStrickler@users.noreply.github.com>
2024-04-25 08:21:41 +0200

    fb_added-unit-tests (#30)
    * feat: Add language support check in LanguageController
    
    Added a check to ensure that the selected language is supported before changing it. Also added validation to prevent adding duplicate observers.
    
    * feat: Add LanguageChangeObserver to MainApplication
    
    Add LanguageChangeObserver interface to MainApplication class to be able to
    listen to language change events. Implement onLanguageChange method to update
    the ResourceBundle and set the stage title accordingly. Also, inject
    LanguageController dependency into SignUpManager.
    
    * moved file
    
    * feat: Implement language change handling in SignUpManager
    
    Add method to handle language changes in SignUpManager. Update dialog
    title and resources when language changes. Initialize LanguageController
    and ResourceBundle in SignUpManager constructor. Add LanguageChangeObserver
    implementation to SignUpManager.
    
    * test: Add tests for SignUpManager and LanguageController
    
    Added test cases to ensure proper functionality of SignUpManager and
    LanguageController classes. Tests include initializing dialog stage,
    setting language, adding/removing observers, notifying observers, and
    checking supported language.
    
    * test: Refactor SignUpManagerTest
    
    Removed unnecessary comments and imports for better code readability.

--- Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/MainApplication.java
+++ Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/MainApplication.java
@@ -1,5 +1,6 @@
 package com.bad_walden_stadtwerke;
 
+import com.bad_walden_stadtwerke.ui.controller.LanguageChangeObserver;
 import com.bad_walden_stadtwerke.ui.controller.LanguageController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,20 +8,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import com.bad_walden_stadtwerke.ui.controller.SignUpManager;
 
 
-public class MainApplication extends Application {
+public class MainApplication extends Application implements LanguageChangeObserver {
 
-    static Stage stage;
-    private final SignUpManager signUpController = new SignUpManager();
-
+    private ResourceBundle bundle = ResourceBundle.getBundle("Bundle", LanguageController.getLanguage());
+    private Stage stage;
     @Override
     public void start(Stage stage) throws IOException {
-        MainApplication.stage = stage;
-        switchScene();
-        stage.setTitle("Stadtwerke Bad Walden");
+        this.stage = stage;
+        SignUpManager signUpController = new SignUpManager(new Stage(), new FXMLLoader(), new LanguageController());
+        switchScene(this.stage);
+        this.stage.setTitle(bundle.getString("mainApplicationTitle"));
+
+        LanguageController.addObserver(this);
 
         signUpController.checkAndOpenForSignUp();
     }
@@ -29,7 +33,7 @@ public class MainApplication extends Application {
         launch(args);
     }
 
-    public static void switchScene() throws IOException {
+    public void switchScene(Stage stage) throws IOException {
         ResourceBundle languageBundle = ResourceBundle.getBundle("Bundle", LanguageController.getLanguage());
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("mainApplication/main-application.fxml"));
@@ -39,6 +43,11 @@ public class MainApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setScene(scene);
         stage.show();
+    }
 
+    @Override
+    public void onLanguageChange(Locale newLocale) {
+        bundle = ResourceBundle.getBundle("Bundle", newLocale);
+        stage.setTitle(bundle.getString("mainApplicationTitle"));
     }
 }
--- Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/ui/controller/SignUpManager.java
+++ Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/ui/controller/SignUpManager.java
@@ -8,9 +8,10 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class SignUpManager {
+public class SignUpManager implements LanguageChangeObserver{
 
     private static final String SIGN_UP_DIALOG_PATH = "/com/bad_walden_stadtwerke/initialSignUp/signup-dialog-0.fxml";
     public static final String BUNDLE_NAME = "Bundle";
@@ -18,16 +19,24 @@ public class SignUpManager {
     private static final int SCENE_HEIGHT = 550;
 
     private final Stage dialogStage;
+    private final FXMLLoader fxmlLoader;
+    private final LanguageController languageController;
+    private ResourceBundle bundle;
 
-    public SignUpManager() {
-        dialogStage = new Stage();
+    public SignUpManager(Stage stage, FXMLLoader fxmlLoader, LanguageController languageController) {
+        LanguageController.addObserver(this);
+        this.dialogStage = stage;
+        this.fxmlLoader = fxmlLoader;
+        this.languageController = languageController;
+        bundle = ResourceBundle.getBundle(BUNDLE_NAME, LanguageController.getLanguage());
+        fxmlLoader.setResources(ResourceBundle.getBundle(BUNDLE_NAME, LanguageController.getLanguage()));
         initializeDialogStage();
     }
 
     private void initializeDialogStage() {
         dialogStage.initModality(Modality.APPLICATION_MODAL);
         dialogStage.initStyle(StageStyle.UNDECORATED);
-        dialogStage.setTitle("Sign Up");
+        dialogStage.setTitle(bundle.getString("signUpPopUpTitle"));
     }
 
     public void checkAndOpenForSignUp() {
@@ -35,6 +44,8 @@ public class SignUpManager {
             openSignUpDialog();
         }
     }
+
+
 
     private void openSignUpDialog() {
         Parent dialogRoot = loadFXML();
@@ -47,8 +58,7 @@ public class SignUpManager {
     }
 
     private Parent loadFXML() {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(SIGN_UP_DIALOG_PATH));
-        fxmlLoader.setResources(ResourceBundle.getBundle(BUNDLE_NAME, LanguageController.getLanguage()));
+        fxmlLoader.setLocation(getClass().getResource(SIGN_UP_DIALOG_PATH));
         try {
             return fxmlLoader.load();
         } catch (IOException e) {
@@ -60,5 +70,12 @@ public class SignUpManager {
     private boolean hasUserSignedUp() {
         //TODO: Replace with the user's signup status @joscha-st from mock
         return false;
+    }
+
+    @Override
+    public void onLanguageChange(Locale newLocale) {
+        fxmlLoader.setResources(ResourceBundle.getBundle(BUNDLE_NAME, LanguageController.getLanguage()));
+        bundle = ResourceBundle.getBundle(BUNDLE_NAME, LanguageController.getLanguage());
+        dialogStage.setTitle(bundle.getString("signUpPopUpTitle"));
     }
 }
