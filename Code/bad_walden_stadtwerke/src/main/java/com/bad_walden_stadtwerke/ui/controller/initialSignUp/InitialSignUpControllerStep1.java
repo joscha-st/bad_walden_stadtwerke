d0e06492f1b16593ecb45421f71c943e0c208eed
LukasStrickler <email@lukasstrickler.com>
2024-04-29 10:32:34 +0200

    refactor: Remove unused code and dependencies in InitialSignUpControllerStep1
    Removed the unused method initialize() and the unused import and field related to Tariff in InitialSignUpControllerStep1. This cleanup improves code readability and reduces unnecessary dependencies.

--- Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/ui/controller/initialSignUp/InitialSignUpControllerStep1.java
+++ Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/ui/controller/initialSignUp/InitialSignUpControllerStep1.java
@@ -2,17 +2,13 @@ package com.bad_walden_stadtwerke.ui.controller.initialSignUp;
 
 import com.bad_walden_stadtwerke.communication.StandardOutboundRequestHandler;
 import com.bad_walden_stadtwerke.logic.BillingAddress;
-import com.bad_walden_stadtwerke.sales.types.Tariff;
 import com.bad_walden_stadtwerke.ui.controller.FXMLUtility;
-import com.bad_walden_stadtwerke.ui.controller.LanguageChangeObserver;
 import com.bad_walden_stadtwerke.ui.controller.LanguageController;
 import com.bad_walden_stadtwerke.ui.controller.SignUpManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
-import java.util.ArrayList;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 import static com.bad_walden_stadtwerke.ui.components.errorHandling.ExceptionPopup.showErrorPopup;
@@ -22,7 +18,6 @@ public class InitialSignUpControllerStep1  {
     private static final String FXML_PATH = "/com/bad_walden_stadtwerke/initialSignUp/signup-dialog-2.fxml";
     private static final String BUNDLE_NAME = SignUpManager.BUNDLE_NAME;
     private ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME, LanguageController.getLanguage());
-    private ArrayList<Tariff> water = new ArrayList<>();
 
     @FXML
     private TextField firstName;
@@ -37,10 +32,6 @@ public class InitialSignUpControllerStep1  {
     @FXML
     private TextField city;
 
-    @FXML
-    public void initialize() {
-        water = (ArrayList<Tariff>) StandardOutboundRequestHandler.makeTariffOutboundRequest("electricity");
-    }
 
     @FXML
     public void next(ActionEvent event) {
