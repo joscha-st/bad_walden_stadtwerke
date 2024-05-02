ecf4600f75c52be9b93bce2762f065a3da252370
LukasStrickler <email@lukasstrickler.com>
2024-05-02 12:43:58 +0200

    added error popup on no checkbox selected

--- Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/controller/initialSignUp/PopupController.java
+++ Code/bad_walden_stadtwerke/src/main/java/com/bad_walden_stadtwerke/controller/initialSignUp/PopupController.java
@@ -1,11 +1,19 @@
 package com.bad_walden_stadtwerke.controller.initialSignUp;
 
+import com.bad_walden_stadtwerke.components.errorHandling.ExceptionPopup;
+import com.bad_walden_stadtwerke.controller.language.LanguageController;
+import com.bad_walden_stadtwerke.model.initialSignUp.SignUpManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.util.ResourceBundle;
+
+
 public class PopupController {
+	private static final String BUNDLE_NAME = SignUpManager.BUNDLE_NAME;
+
 	@FXML
 	private CheckBox checkBox;
 	@FXML
@@ -32,6 +40,10 @@ public class PopupController {
 
 	@FXML
 	public void confirm() {
+		if (!checkBox.isSelected()) {
+			ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME, LanguageController.getLanguage());
+			ExceptionPopup.showErrorPopup(bundle.getString("tariffSelectionConfirmationErrorLabel"), bundle.getString("tariffSelectionConfirmationNoCheckoxErrorLabel"));
+		}
 		Stage stage = (Stage) checkBox.getScene().getWindow();
 		stage.close();
 	}
